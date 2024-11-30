2e892a1db6c4ef2f87d80d309f56cdd9daf75e81
Philip Nielsen <71400793+philip-nielsen@users.noreply.github.com>
2024-11-30 22:18:58 +0100

    Fix main filen för task 5

--- assignment1/assignment/src/main/java/assignment1/tasks/task5/Task5.java
+++ assignment1/assignment/src/main/java/assignment1/tasks/task5/Task5.java
@@ -1,68 +1,64 @@
-package assignment1.assignment.src.main.java.assignment1.tasks.task5;
-
-import assignment1.assignment.src.main.java.assignment1.processinteraction.Global;
-import assignment1.assignment.src.main.java.assignment1.processinteraction.Signal;
-import assignment1.assignment.src.main.java.assignment1.processinteraction.SignalList;
-
 import java.util.*;
 import java.io.*;
 
 //It inherits Proc so that we can use time and the signal names without dot notation
 
-public class Task5 extends Global {
+public class MainSimulation extends Global {
 
-    public void run() throws IOException {
-        System.out.println("Task 5");
-        // The signal list is started and actSignal is declaree. actSignal is the latest
-        // signal that has been fetched from the
-        // signal list in the main loop below.
+	public static void main(String[] args) throws IOException {
 
-        Signal actSignal;
-        new SignalList();
+		// The signal list is started and actSignal is declaree. actSignal is the latest
+		// signal that has been fetched from the
+		// signal list in the main loop below.
 
-        TaskQS Q1 = new TaskQS();
-        Q1.sendTo = null;
-        TaskQS Q2 = new TaskQS();
-        Q2.sendTo = null;
-        TaskQS Q3 = new TaskQS();
-        Q3.sendTo = null;
-        TaskQS Q4 = new TaskQS();
-        Q4.sendTo = null;
-        TaskQS Q5 = new TaskQS();
-        Q5.sendTo = null;
+		Signal actSignal;
+		new SignalList();
 
-        TaskGen Generator = new TaskGen();
-        Generator.lambda = 2.0; //0.11 or 0.15 or 2.0
-        Generator.sendToList = new ArrayList<TaskQS>(Arrays.asList(Q1, Q2, Q3, Q4, Q5));
-        ;
+		QS Q1 = new QS();
+		Q1.sendTo = null;
+		QS Q2 = new QS();
+		Q2.sendTo = null;
+		QS Q3 = new QS();
+		Q3.sendTo = null;
+		QS Q4 = new QS();
+		Q4.sendTo = null;
+		QS Q5 = new QS();
+		Q5.sendTo = null;
 
-        // To start the simulation the first signals are put in the signal list
+		Gen Generator = new Gen();
+		Generator.mean = 0.15;
+		Generator.sendToList = new ArrayList<QS>(Arrays.asList(Q1, Q2, Q3, Q4, Q5));
 
-        SignalList.SendSignal(READY, Generator, time);
-        SignalList.SendSignal(MEASURE, Q1, time);
-        SignalList.SendSignal(MEASURE, Q2, time);
-        SignalList.SendSignal(MEASURE, Q3, time);
-        SignalList.SendSignal(MEASURE, Q4, time);
-        SignalList.SendSignal(MEASURE, Q5, time);
+		// To start the simulation the first signals are put in the signal list
 
-        // This is the main loop
+		SignalList.SendSignal(READY, Generator, time);
+		SignalList.SendSignal(MEASURE, Generator, 0);
+		SignalList.SendSignal(MEASURE, Q1, 0);
+		SignalList.SendSignal(MEASURE, Q2, 0);
+		SignalList.SendSignal(MEASURE, Q3, 0);
+		SignalList.SendSignal(MEASURE, Q4, 0);
+		SignalList.SendSignal(MEASURE, Q5, 0);
 
-        while (time < 100000) {
-            actSignal = SignalList.FetchSignal();
-            time = actSignal.arrivalTime;
-            actSignal.destination.TreatSignal(actSignal);
-        }
+		// This is the main loop
 
-        // Finally the result of the simulation is printed below:
+		while (time < 100000) {
+			actSignal = SignalList.FetchSignal();
+			time = actSignal.arrivalTime;
+			actSignal.destination.TreatSignal(actSignal);
+		}
 
-        System.out.println("Mean nmbr customers sys 1: " + 1.0 * Q1.accumulated / Q1.noMeasurements);
-        System.out.println("Mean nmbr customers sys 2: " + 1.0 * Q2.accumulated / Q2.noMeasurements);
-        System.out.println("Mean nmbr customers sys 3: " + 1.0 * Q3.accumulated / Q3.noMeasurements);
-        System.out.println("Mean nmbr customers sys 4: " + 1.0 * Q4.accumulated / Q4.noMeasurements);
-        System.out.println("Mean nmbr customers sys 5: " + 1.0 * Q5.accumulated / Q5.noMeasurements);
-        System.out.println("Mean nmbr customers total: " + 1.0
-                * (Q1.accumulated + Q2.accumulated + Q3.accumulated + Q4.accumulated + Q5.accumulated)
-                / (Q1.noMeasurements + Q2.noMeasurements + Q3.noMeasurements + Q4.noMeasurements + Q5.noMeasurements));
-        System.out.println("----------------------------------------");
-    }
+		// Finally the result of the simulation is printed below:
+
+		System.out.println(
+				"Total and numberOfMeasurments: " + Generator.totalInQueues + " " + Generator.numberOfMeasurements);
+		System.out.println(
+				"Total Mean numbr of customers: " + 1.0 * Generator.totalInQueues / Generator.numberOfMeasurements);
+
+		System.out.println(
+				"Mean nmbr customers sys 1: " + 1.0 * Generator.numberInQueues.get(0) / Generator.numberOfMeasurements);
+		System.out.println("Mean nmbr customers sys 2: " + 1.0 * Q2.accumulated / Q2.noMeasurements);
+		System.out.println("Mean nmbr customers sys 3: " + 1.0 * Q3.accumulated / Q3.noMeasurements);
+		System.out.println("Mean nmbr customers sys 4: " + 1.0 * Q4.accumulated / Q4.noMeasurements);
+		System.out.println("Mean nmbr customers sys 5: " + 1.0 * Q5.accumulated / Q5.noMeasurements);
+	}
 }
