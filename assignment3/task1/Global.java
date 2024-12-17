6adce817cbf0ef7a7849cd44ae9a984a3d436627
emillantz <emil.lantz@gmail.com>
2024-12-17 23:31:26 +0100

    remove old a3t1

--- assignment3/task1/Global.java
+++
@@ -1,4 +0,0 @@
-public class Global{
-	public static final int SEND_MESSAGE = 1, SEND_CHECK = 2, MEASURE = 3, RECEIVE_TRANSMISSION = 4, TRANSMISSION_END = 5, CHECK_CHANNEL = 6;
-	public static double time = 0;
-}
